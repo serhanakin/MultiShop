66235f0d848c225071d05a38bb5aeb5d1a25b1a0
Serhan Akin <srhnakn.dev@gmail.com>
2026-03-23 21:25:57 +0300

    Add Discount service with Coupon entity, EF Core, and Swagger
    Introduced a new Discount (Coupon) management feature using Dapper and Entity Framework Core. Added Coupon entity, related DTOs, and database context with SQL Server support. Included initial EF Core migrations for Coupons table. Implemented service interfaces and stubs for coupon operations. Removed WeatherForecast sample code. Enabled Swagger UI for API documentation. Updated project dependencies and configuration files to support the new service.

---
+++ Services/Discount/MultiShop.Discount/Context/DapperContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MultiShop.Discount.Entities;
+using System.Data;
+
+namespace MultiShop.Discount.Context
+{
+    public class DapperContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS01;Database=MultiShopDiscountDb;Integrated Security=True;TrustServerCertificate=True;");
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+
+        public IDbConnection CreateConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
