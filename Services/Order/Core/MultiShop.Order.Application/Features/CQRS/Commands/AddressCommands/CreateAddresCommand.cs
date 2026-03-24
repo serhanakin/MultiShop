2a808fc16607cc37d0a54c99587b30b11b6e8f65
Serhan Akin <srhnakn.dev@gmail.com>
2026-03-24 11:27:13 +0300

    feat: CQRS files added.

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Commands/AddressCommands/CreateAddresCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiShop.Order.Application.Features.CQRS.Commands.AddressCommands
+{
+    public class CreateAddresCommand
+    {
+        public string UserId { get; set; }
+        public string District { get; set; }
+        public string City { get; set; }
+        public string Detail { get; set; }
+    }
+}
---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/CreateAddressCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Commands.AddressCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers
+{
+    public class CreateAddressCommandHandler
+    {
+        private readonly IRepository<Address> _repository;
+
+        public CreateAddressCommandHandler(IRepository<Address> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(CreateAddresCommand request)
+        {
+            var address = new Address
+            {
+                UserId = request.UserId,
+                District = request.District,
+                City = request.City,
+                Detail = request.Detail
+            };
+            await _repository.CreateAsync(address);
+        }
+    }
+}
