73bb19d6be7b7d189d4c63bb94fc7c1c8d29237b
Serhan Akin <srhnakn.dev@gmail.com>
2026-03-24 11:38:25 +0300

    feat: address handlers added.

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/RemoveAddressCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Commands.AddressCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers
+{
+    public class RemoveAddressCommandHandler
+    {
+        private readonly IRepository<Address> _repository;
+
+        public RemoveAddressCommandHandler(IRepository<Address> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(RemoveAddressCommand request)
+        {
+            var address = await _repository.GetByIdAsync(request.Id);
+
+            if (address != null)
+            {
+                await _repository.DeleteAsync(address);
+            }
+        }
+    }
+}
