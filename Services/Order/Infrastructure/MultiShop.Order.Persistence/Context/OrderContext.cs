bedc5d163e33195eec441dbe05dfef9a921ab1df
Serhan Akin <srhnakn.dev@gmail.com>
2026-03-27 11:09:35 +0300

    feat: repository added.

---
+++ Services/Order/Infrastructure/MultiShop.Order.Persistence/Context/OrderContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Persistence.Context
+{
+    public class OrderContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=MultiShopOrderDb;Trusted_Connection=True; InitialCatalog=MultiShopOrderDb");
+        }
+
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Ordering> Orderings { get; set; }
+    }
+}
---
+++ Services/Order/Infrastructure/MultiShop.Order.Persistence/Repositories/Repository.cs
@@ -0,0 +1,50 @@
+﻿using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Persistence.Context;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace MultiShop.Order.Persistence.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly OrderContext _context;
+
+        public Repository(OrderContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateAsync(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public Task GetByFilterAsync(Expression<Func<T, bool>> filter)
+        {
+            return _context.Set<T>().SingleOrDefaultAsync(filter);
+        }
+
+        public Task GetByIdAsync(int id)
+        {
+            return _context.Set<T>().FindAsync(id).AsTask();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
