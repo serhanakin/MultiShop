241a106f4b4ae554f02cf3e90b77396bf3fedd5c
Serhan Akin <srhnakn.dev@gmail.com>
2026-04-06 12:12:37 +0300

    bug fixes.

--- Services/Order/Presentation/MultiShop.Order.WebApi/Controllers/AddressesController.cs
+++ Services/Order/Presentation/MultiShop.Order.WebApi/Controllers/AddressesController.cs
@@ -33,14 +33,14 @@ namespace MultiShop.Order.WebApi.Controllers
         [HttpGet]
         public async Task<IActionResult> AddressList()
         {
-            var values = _getAddressQueryHandler.Handle();
+            var values = await _getAddressQueryHandler.Handle();
             return Ok(values);
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetAddressById(int id)
         {
-            var values = _getAddressByIdQueryHandler.Handle(new GetAddressByIdQuery(id));
+            var values = await _getAddressByIdQueryHandler.Handle(new GetAddressByIdQuery(id));
             return Ok(values);
         }
 
