3064d1d3bd816ab51ac4aec3127859e61f905b73
Serhan Akin <srhnakn.dev@gmail.com>
2026-04-13 10:57:05 +0300

    feat: BasketService added.

---
+++ Services/Basket/MultiShop.Basket/Services/BasketService.cs
@@ -0,0 +1,38 @@
+﻿using MultiShop.Basket.Dtos;
+using MultiShop.Basket.Settings;
+using System.Text.Json;
+
+namespace MultiShop.Basket.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly RedisService _redisservice;
+
+        public BasketService(RedisService redisservice)
+        {
+            _redisservice = redisservice;
+        }
+
+        public async Task DeleteBasket(string userId)
+        {
+            await _redisservice.GetDb().KeyDeleteAsync(userId);
+        }
+
+        public async Task<BasketTotalDto> GetBasket(string userId)
+        {
+            var existBasket = await _redisservice.GetDb().StringGetAsync(userId);
+
+            if (!existBasket.HasValue)
+            {
+                return null;
+            }
+
+            return JsonSerializer.Deserialize<BasketTotalDto>((string)existBasket);
+        }
+
+        public async Task SaveBasket(BasketTotalDto basketTotalDto)
+        {
+            await _redisservice.GetDb().StringSetAsync(basketTotalDto.UserId, JsonSerializer.Serialize(basketTotalDto));
+        }
+    }
+}
