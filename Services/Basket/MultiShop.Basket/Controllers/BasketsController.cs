abdcd24b83e66300475b7425888bd19d9080d3c2
Serhan Akin <srhnakn.dev@gmail.com>
2026-04-13 13:48:05 +0300

    feat: Basket controller has been completed.

--- Services/Basket/MultiShop.Basket/Controllers/BasketsController.cs
+++ Services/Basket/MultiShop.Basket/Controllers/BasketsController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.Basket.Dtos;
+using MultiShop.Basket.LoginServices;
+using MultiShop.Basket.Services;
 
 namespace MultiShop.Basket.Controllers
 {
@@ -6,6 +9,39 @@ namespace MultiShop.Basket.Controllers
     [ApiController]
     public class BasketsController : ControllerBase
     {
+        private readonly IBasketService _basketService;
+        private readonly ILoginService _loginService;
 
+        public BasketsController(ILoginService loginService, IBasketService basketService)
+        {
+            _loginService = loginService;
+            _basketService = basketService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMyBasketDetail()
+        {
+            var values = await _basketService.GetBasket(_loginService.GetUserId);
+            
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveMyBasket(BasketTotalDto basketTotalDto)
+        {
+            basketTotalDto.UserId = _loginService.GetUserId;
+
+            await _basketService.SaveBasket(basketTotalDto);
+
+            return Ok("Basket has been saved successfully!");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteBasket()
+        {
+            await _basketService.DeleteBasket(_loginService.GetUserId);
+
+            return Ok("Basket has been deleted successfully!");
+        }
     }
 }
