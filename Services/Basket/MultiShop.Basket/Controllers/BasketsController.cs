b27838232023c0ff5f3bea93ee71b3afaed200d4
Serhan Akin <srhnakn.dev@gmail.com>
2026-04-13 10:24:25 +0300

    feat: Basket api init.

---
+++ Services/Basket/MultiShop.Basket/Controllers/BasketsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.Basket.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketsController : ControllerBase
+    {
+    }
+}
